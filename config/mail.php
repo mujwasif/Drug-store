880dbd248e976ddec6abcb64c84d50b4030f5882
Rcxtor <sefat64@gmail.com>
2023-11-15 12:21:15 +0600

    Reset password

--- config/mail.php
+++ config/mail.php
@@ -100,8 +100,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 's.ahmed.1416@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Hello Laravel'),
     ],
 
     /*
